ce42dd03ad9fec32baf84f5746af0593c601d5e7
hari <harikrishnaz01>
2023-07-03 22:30:06 +0530

    Program copied

---
+++ pytest.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+
+path = '/home/hari/Downloads'
+
+
+while True:
+	list_ = os.listdir(path)
+	for file_ in list_:
+		name, ext = os.path.splitext(file_)
+
+		ext = ext[1:]
+
+		if ext == " ":
+			continue
+			print()
+		if os.path.exists(path+'/'+ext):
+			shutil.move(path+'/'+file_ , path+'/'+ext+'/'+file_)
+		else :
+			os.makedirs(path+'/'+ext)
+			shutil.move(path+'/'+file_ , path+'/'+ext+'/'+file_)
